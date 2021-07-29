57086512f188070b39caf11921b3e2a6fa395a64
Mitch Strange <mstrange382@gmail.com>
2021-07-29 16:41:43 -0500

    pushing up forked code from robs repo first commit hold blue badge module

--- 05-Node-Server/server/controllers/usercontroller.js
+++ 05-Node-Server/server/controllers/usercontroller.js
@@ -2,23 +2,27 @@ let express = require('express');
 let router = express.Router();
 let sequelize = require('../db')
 let user = sequelize.import('../models/user.js')
+let jwt = require("jsonwebtoken");
 
-// router.post('/create', function(req,res){
+router.post('/create', function(req,res){
 
-//     let userModel = {
-//         email: req.body.user.email,
-//         password: req.body.user.password,
-//     };
-//     user.create(userModel).then(function(user){
-//         let responseObject = {
-//             user: user
-//         };
-//         res.json(responseObject);
-//     })
-//     // .catch(function(err){
-//     //     res.json({error: err})
-//     // });
-// });
+    User.create({
+        email: req.body.user.email,
+        password: req.body.user.password,
+    })
+    .then(function createSuccess(user){
+        let token = jwt.sign({id: user.id}, "i_am_secret", {expiresIn: 60*60*24});
+        res.json( {
+            user: user,
+            message: "user successfully created",
+            sessionToken: token
+        });
+        
+    })
+    .catch(function(err){
+        res.json({error: err})
+    });
+});
 
 router.post("/login", function(req,res){
     user.findOne({where:{email: 'test4@test.com'}})
